21919d7777ab98935fb0f57e353a3f599bdbc690
Gleides Vinente <gleidevelop@gmail.com>
2023-07-13 19:58:50 -0400

    Ajustando os componentes Card e Navbar

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react'
-const CardImage = () =>{
-    return(
+
+const CardImage = () => {
+    return (
         <svg className="bd-placeholder-img card-img-top" width="100%" height="140"
              xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap"
              preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
             <rect width="100%" height="100%" fill="#868e96"></rect>
-            <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+            <image href="https://www.svgrepo.com/show/269884/transportation-big.svg" width="100%" height="100%"/>
         </svg>
+
     );
 }
 
-const CardBody = () =>{
+const CardBody = () => {
     return (
         <div className="card">
             <CardImage/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,11 +57,6 @@ const NavbarBody = () => {
         <div className="container-fluid">
             {/* eslint-disable-next-line*/}
             <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             <NavbarBodyContent/>
         </div>
     );
