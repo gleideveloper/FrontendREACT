8f7018bb4bf4a21ca2c2dc99c5820f95c996017b
Gleides Vinente <gleidevelop@gmail.com>
2023-07-18 16:32:47 -0400

    Adicionado a prop addToCart na class CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,11 +3,11 @@ import Card from "./Card";
 
 class CardList extends Component {
     render() {
-        const { characterData } = this.props;
+        const { characterData, addToCart } = this.props;
         return (
             <div className="row row-cols-1 row-cols-md-3 g-2">
                 {characterData.map((item, index) => (
-                    <Card key={index} item={item} />
+                    <Card key={index} item={item} addToCart={() => addToCart(item)} />
                 ))}
             </div>
         );
